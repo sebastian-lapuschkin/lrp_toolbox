dee4197ab8fdba75aead17be0ddcb9175b94c78f
Sebastian Lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2016-11-10 12:55:03 +0100

    lrp for Convolution.m

--- matlab/+modules/Convolution.m
+++ matlab/+modules/Convolution.m
@@ -20,8 +20,6 @@ classdef Convolution < modules.Module
         %temporary variables
         Y
         X
-        DB
-        DW
         DY
     end
 
@@ -169,7 +167,6 @@ classdef Convolution < modules.Module
             obj.W = obj.W - lrate .* DW;
             obj.B = obj.B - lrate .* DB;
             
-            
         end
        
         
@@ -180,33 +177,41 @@ classdef Convolution < modules.Module
         end
 
         
-%         
-%         function Rx = lrp(obj,R,varargin)
-%             % LRP according to Eq(56) in DOI: 10.1371/journal.pone.0130140
-%             [N,H,W,D] = size(obj.X);
-% 
-%             hpool = obj.pool(1);        wpool = obj.pool(2);
-%             hstride = obj.stride(1);    wstride = obj.stride(2);
-% 
-%             %assume the given pooling and stride parameters are carefully
-%             %chosen
-%             Hout = (H - hpool)/hstride + 1;
-%             Wout = (W - wpool)/wstride + 1;
-%             
-%             Rx = zeros(N,H,D,W);
-%             for i = 1:Hout
-%                 for j = 1:Wout
-%                     Z = obj.X(: , (i-1)*hstride+1:(i-1)*hstride+hpool , (j-1)*wstride+1:(j-1)*wstride+wpool , :);
-%                     Zs = sum(sum(Z,2),3);
-%                     Zs = Zs + 1e-12 .* ((Zs >= 0) .* 2 -1); % add a weak numerical stabilizer to avoid zero division
-%                     
-%                     rr = repmat(R(:,i,j,:),[1,hpool,wpool,1]);
-%                     zz = Z ./ repmat(Zs,[1,hpool,wpool,1]);
-%                     rx = Rx(: , (i-1)*hstride+1:(i-1)*hstride+hpool , (j-1)*wstride+1:(j-1)*wstride+wpool , :);
-%                     
-%                     Rx(: , (i-1)*hstride+1:(i-1)*hstride+hpool , (j-1)*wstride+1:(j-1)*wstride+wpool , :) = rx + rr .* zz;
-%                 end
-%             end
-%         end
+        
+       
+        function Rx = lrp(obj,R,varargin)
+            % LRP according to Eq(56) in DOI: 10.1371/journal.pone.0130140
+            
+            [N,Hx,Wx,df] = size(obj.X);
+            [N,Hout,Wout,Nf] = size(R);
+            [hf,wf,df,Nf] = size(obj.W);
+            hstride = obj.stride(1);    wstride = obj.stride(2);
+            
+            
+            %prepare W and B for the loop below
+            Wr = reshape(obj.W,[1 obj.filtersize]);
+            Wr = repmat(Wr,[N 1 1 1 1]);
+            Br = reshape(obj.B,[1 numfilters]);
+            Br = repmat(Br,[N 1]);
+            
+            Rx = zeros(N,Hx,Wx,df);
+            for i = 1:Hout
+                for j = 1:Wout
+                    x = obj.X(:,(i-1)*hstride+1:(i-1)*hstride+hf,(j-1)*wstride+1:(j-1)*wstride+wf,:);
+                    x = repmat(x,[1 1 1 1 Nf]);
+                    Z = Wr .* x; % N x hf x wf x df x Nf
+                    
+                    Zs = sum(sum(sum(Z,2),3),4) + Br ; % N x Nf
+                    Zs = Zs + 1e-12*((Zs >= 0)*2 -1); %add a weak numerical stabilizer to cushion division by zero
+                    Zs = repmat(reshape(Zs,[N 1 1 1 Nf]),[1 hf wf df Nf]); % N x hf x wf x df x Nf
+                    
+                    zz = Z ./ Zs ; % N x hf x wf x df x Nf
+                    rr = repmat(reshape(R(:,i,j,:),[N 1 1 1 Nf]),[1 hf wf df 1]); % N x hf x wf x df x Nf
+                    rx = Rx(:,(i-1)*hstride+1:(i-1)*hstride+hf,(j-1)*wstride+1:(j-1)*wstride+wf,:); % N x hf x wf x df
+                    
+                    Rx(:,(i-1)*hstride+1:(i-1)*hstride+hf,(j-1)*wstride+1:(j-1)*wstride+wf,:) = rx +  sum(zz .* rr,5);
+                end
+            end
+        end
     end
 end
