36ddde2e56abd3efb0c2da9ce95bb6a54943a83d
lapuschkin <sebastian.lapuschkin@hhi.fraunhofer.de>
2017-11-30 17:43:17 +0100

    smaller batches for testing during coding

--- python/lrp_timing_cnn.py
+++ python/lrp_timing_cnn.py
@@ -42,8 +42,8 @@ I = np.arange(X.shape[0])
 
 # do some benchmarking.
 
-#for B in [1, 16, 64]: #batch sizes for work laptop with only 8GB RAM
-for B in [1, 16, 64, 256]:
+for B in [1, 16, 64]: #batch sizes for work laptop with only 8GB RAM
+#for B in [1, 16, 64, 256]:
 
     forward_times_old = []
     forward_times_new = []
@@ -77,24 +77,24 @@ for B in [1, 16, 64, 256]:
         t_start = time.time();  yold = nn.forward(x);                               forward_times_old.append(time.time() - t_start)
         t_start = time.time();  Rold = nn.lrp(yold, 'simple_slow');                 lrp_times_old.append(time.time() - t_start)
         t_start = time.time();  REold = nn.lrp(yold, 'epsilon_slow', 0.01);         lrp_times_old2.append(time.time() - t_start)
-        #t_start = time.time();  RAold2 = nn.lrp(yold, 'alphabeta_slow', 2.);          lrp_times_old3.append(time.time() - t_start)
-        #t_start = time.time();  RAold1 = nn.lrp(yold, 'alphabeta_slow', 1.);          lrp_times_old4.append(time.time() - t_start)
-        #t_start = time.time();  RAold0 = nn.lrp(yold, 'alphabeta_slow', 0.);          lrp_times_old5.append(time.time() - t_start)
+        t_start = time.time();  RAold2 = nn.lrp(yold, 'alphabeta_slow', 2.);          lrp_times_old3.append(time.time() - t_start)
+        t_start = time.time();  RAold1 = nn.lrp(yold, 'alphabeta_slow', 1.);          lrp_times_old4.append(time.time() - t_start)
+        t_start = time.time();  RAold0 = nn.lrp(yold, 'alphabeta_slow', 0.);          lrp_times_old5.append(time.time() - t_start)
         # newer lrp code
         t_start = time.time();  ynew = nn.forward(x);                               forward_times_new.append(time.time() - t_start)
         t_start = time.time();  Rnew = nn.lrp(ynew, 'simple');                      lrp_times_new.append(time.time() - t_start)
         t_start = time.time();  REnew = nn.lrp(ynew, 'epsilon', 0.01);              lrp_times_new2.append(time.time() - t_start)
-        #t_start = time.time();  RAnew2 = nn.lrp(ynew, 'alphabeta', 2.);              lrp_times_new3.append(time.time() - t_start)
-        #t_start = time.time();  RAnew1 = nn.lrp(ynew, 'alphabeta', 1.);              lrp_times_new4.append(time.time() - t_start)
-        #t_start = time.time();  RAnew0 = nn.lrp(ynew, 'alphabeta', 0.);              lrp_times_new5.append(time.time() - t_start)
+        t_start = time.time();  RAnew2 = nn.lrp(ynew, 'alphabeta', 2.);              lrp_times_new3.append(time.time() - t_start)
+        t_start = time.time();  RAnew1 = nn.lrp(ynew, 'alphabeta', 1.);              lrp_times_new4.append(time.time() - t_start)
+        t_start = time.time();  RAnew0 = nn.lrp(ynew, 'alphabeta', 0.);              lrp_times_new5.append(time.time() - t_start)
 
         # lrp aware code and forward pass
         t_start = time.time();  yaw = nn.forward(x, lrp_aware=True);                forward_times_aware.append(time.time() - t_start)
         t_start = time.time();  Raw = nn.lrp(yaw, 'simple');                        lrp_times_aware.append(time.time() - t_start)
         t_start = time.time();  REaw= nn.lrp(yaw, 'epsilon', 0.01);                 lrp_times_aware2.append(time.time() - t_start)
-        #t_start = time.time();  RAaw2= nn.lrp(yaw, 'alphabeta', 2.);                 lrp_times_aware3.append(time.time() - t_start)
-        #t_start = time.time();  RAaw1= nn.lrp(yaw, 'alphabeta', 1.);                 lrp_times_aware4.append(time.time() - t_start)
-        #t_start = time.time();  RAaw0= nn.lrp(yaw, 'alphabeta', 0.);                 lrp_times_aware5.append(time.time() - t_start)
+        t_start = time.time();  RAaw2= nn.lrp(yaw, 'alphabeta', 2.);                 lrp_times_aware3.append(time.time() - t_start)
+        t_start = time.time();  RAaw1= nn.lrp(yaw, 'alphabeta', 1.);                 lrp_times_aware4.append(time.time() - t_start)
+        t_start = time.time();  RAaw0= nn.lrp(yaw, 'alphabeta', 0.);                 lrp_times_aware5.append(time.time() - t_start)
 
         tolerance = 1e-6
         np.testing.assert_allclose(yold, ynew, rtol=tolerance) # predictions
@@ -106,14 +106,14 @@ for B in [1, 16, 64, 256]:
         np.testing.assert_allclose(REold, REnew, rtol=tolerance) # eps lrp maps
         np.testing.assert_allclose(REold, REaw, rtol=tolerance)
 
-        #np.testing.assert_allclose(RAold2, RAnew2, rtol=tolerance) # alpha2 lrp maps
-        #np.testing.assert_allclose(RAold2, RAaw2, rtol=tolerance)
+        np.testing.assert_allclose(RAold2, RAnew2, rtol=tolerance) # alpha2 lrp maps
+        np.testing.assert_allclose(RAold2, RAaw2, rtol=tolerance)
 
-        #np.testing.assert_allclose(RAold1, RAnew1, rtol=tolerance) # alpha1 lrp maps
-        #np.testing.assert_allclose(RAold1, RAaw1, rtol=tolerance)
+        np.testing.assert_allclose(RAold1, RAnew1, rtol=tolerance) # alpha1 lrp maps
+        np.testing.assert_allclose(RAold1, RAaw1, rtol=tolerance)
 
-        #np.testing.assert_allclose(RAold0, RAnew0, rtol=tolerance) # alpha0 lrp maps
-        #np.testing.assert_allclose(RAold0, RAaw0, rtol=tolerance)
+        np.testing.assert_allclose(RAold0, RAnew0, rtol=tolerance) # alpha0 lrp maps
+        np.testing.assert_allclose(RAold0, RAaw0, rtol=tolerance)
 
         print '.'
 
@@ -129,7 +129,7 @@ for B in [1, 16, 64, 256]:
     print '      old:  ', np.mean(lrp_times_old2), '({}% speedup vs old)'.format(int(100*(1 - np.mean(lrp_times_old2)/np.mean(lrp_times_old2))))
     print '      new:  ', np.mean(lrp_times_new2), '({}% speedup vs old)'.format(int(100*(1 - np.mean(lrp_times_new2)/np.mean(lrp_times_old2))))
     print '      aware:', np.mean(lrp_times_aware2), '({}% speedup vs old)'.format(int(100*(1 - np.mean(lrp_times_aware2)/np.mean(lrp_times_old2))))
-    """
+
     print '    Mean LRP times 3 (alpha=2 lrp):'
     print '      old:  ', np.mean(lrp_times_old3), '({}% speedup vs old)'.format(int(100*(1 - np.mean(lrp_times_old3)/np.mean(lrp_times_old3))))
     print '      new:  ', np.mean(lrp_times_new3), '({}% speedup vs old)'.format(int(100*(1 - np.mean(lrp_times_new3)/np.mean(lrp_times_old3))))
@@ -142,7 +142,7 @@ for B in [1, 16, 64, 256]:
     print '      old:  ', np.mean(lrp_times_old5), '({}% speedup vs old)'.format(int(100*(1 - np.mean(lrp_times_old5)/np.mean(lrp_times_old5))))
     print '      new:  ', np.mean(lrp_times_new5), '({}% speedup vs old)'.format(int(100*(1 - np.mean(lrp_times_new5)/np.mean(lrp_times_old5))))
     print '      aware:', np.mean(lrp_times_aware5), '({}% speedup vs old)'.format(int(100*(1 - np.mean(lrp_times_aware5)/np.mean(lrp_times_old5))))
-    """
+
     print '    Mean Total times with LRP once:'
     oldtotal = np.mean(np.array(lrp_times_old) + np.array(forward_times_old))
     newtotal = np.mean(np.array(lrp_times_new) + np.array(forward_times_new))
@@ -157,7 +157,7 @@ for B in [1, 16, 64, 256]:
     print '      old:  ', oldtotaltwice, '({}% speedup vs old)'.format(int(100*(1 - oldtotaltwice/oldtotaltwice)))
     print '      new:  ', newtotaltwice, '({}% speedup vs old)'.format(int(100*(1 - newtotaltwice/oldtotaltwice)))
     print '      aware:', awaretotaltwice, '({}% speedup vs old)'.format(int(100*(1 - awaretotaltwice/oldtotaltwice)))
-    """
+
     print '    Mean Total times with LRP five times(simple,epsilon,alpha=2,alpha=1,alpha=0):'
     oldtotalfive = oldtotaltwice + np.mean(np.array(lrp_times_old3) + np.array(lrp_times_old4) + np.array(lrp_times_old5))
     newtotalfive = newtotaltwice +  np.mean(np.array(lrp_times_new3) + np.array(lrp_times_new4) + np.array(lrp_times_new5))
@@ -165,7 +165,7 @@ for B in [1, 16, 64, 256]:
     print '      old:  ', oldtotalfive, '({}% speedup vs old)'.format(int(100*(1 - oldtotalfive/oldtotalfive)))
     print '      new:  ', newtotalfive, '({}% speedup vs old)'.format(int(100*(1 - newtotalfive/oldtotalfive)))
     print '      aware:', awaretotalfive, '({}% speedup vs old)'.format(int(100*(1 - awaretotalfive/oldtotalfive)))
-    """
+
     print ''
     print ''
 
