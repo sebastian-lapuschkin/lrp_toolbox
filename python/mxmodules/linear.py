e49f1cd011db5e6b4ef0e287cc36979de810f689
Maximilian Kohlbrenner <maximilian.kohlbrenner@hhi.fraunhofer.de>
2017-12-15 09:47:10 +0100

    Add translated lrp formulas to linear.py

--- python/mxmodules/linear.py
+++ python/mxmodules/linear.py
@@ -9,10 +9,9 @@
 @license : BSD-2-Clause
 '''
 
-from module import Module
-
 import mxnet as mx
 from mxnet import nd
+from module import Module
 
 # -------------------------------
 # Linear layer
@@ -37,7 +36,6 @@ class Linear(Module):
         -------
         the newly created object instance
         '''
-
         Module.__init__(self)
         self.m = m
         self.n = n
@@ -45,7 +43,7 @@ class Linear(Module):
         self.W = nd.random_normal(0,1.0*m**(-.5),[self.m,self.n], ctx=ctx)
 
 
-    def forward(self,X):
+    def forward(self,X,lrp_aware=False):
         '''
         Forward-transforms an input X
 
@@ -60,9 +58,15 @@ class Linear(Module):
         Y : numpy.ndarray
             the transformed data shaped [N,M], with M being the number of output neurons
         '''
+        self.lrp_aware = lrp_aware
+        if self.lrp_aware:
+            self.X = X
+            self.Z = nd.expand_dims(self.W, axis=0) * nd.expand_dims(self.X, axis=2)
+            self.Y = nd.sum(self.Z, axis=1) + self.B
+        else:
+            self.X = X
+            self.Y = nd.dot(X,self.W)+self.B
 
-        self.X = X
-        self.Y = nd.dot(X,self.W)+self.B
         return self.Y
 
 
@@ -86,7 +90,7 @@ class Linear(Module):
 
         self.dW = nd.dot(self.X.T,DY)
         self.dB = nd.sum(DY, axis=0)
-        return nd.dot(DY,self.W.T)*self.m**.5/self.n**.5
+        return nd.dot(DY,self.W.T) *self.m**.5/self.n**.5
 
 
     def update(self, lrate):
@@ -107,67 +111,142 @@ class Linear(Module):
         self.dB = None
 
 
+    def _simple_lrp_slow(self,R):
+        '''
+        LRP according to Eq(56) in DOI: 10.1371/journal.pone.0130140
+        This function shows all necessary operations to perform LRP in one place and is therefore not optimized
+        '''
+        Z = nd.expand_dims(self.W, axis=0) * nd.expand_dims(self.X, axis=2) #localized preactivations
+        Zs = nd.expand_dims(nd.sum(Z, axis=1), axis=1) + nd.expand_dims(nd.expand_dims(self.B, axis=0), axis=0)
+        Zs += 1e-16*((Zs >= 0)*2 - 1.) #add weak default stabilizer to denominator
+        return nd.sum((Z / Zs) * nd.expand_dims(R, axis=1), axis=2)
+
+
     def _simple_lrp(self,R):
         '''
         LRP according to Eq(56) in DOI: 10.1371/journal.pone.0130140
         '''
 
-        bs = self.X.shape[0] # batchsize
-        Z  = self.W.reshape((1,self.m,self.n)) * self.X.reshape((bs, self.m, 1)) # localized preactivations
-        Zs = nd.sum(Z, axis=1).reshape((bs, 1, self.n)) + self.B.reshape((1,1,self.n))
-        return nd.sum((Z / Zs) * R.reshape((bs, 1, self.n)), axis=2)
+        # Has the forward pass been computed lrp-aware?
+        # This exchanges time spent in the forward pass for lower LRP time
+        # and is useful, if e.g. several parameter settings for LRP need to be evaluated
+        # for the same input data.
+        Zs = self.Y + 1e-16*((self.Y >= 0)*2 - 1.) #add weakdefault stabilizer to denominator
+        if self.lrp_aware:
+            return nd.sum(self.Z * nd.expand_dims(R/Zs, 1), axis=2)
+        else:
+            Z = nd.expand_dims(self.W, axis=0) * nd.expand_dims(self.X, axis=2) #localized preactivations
+            return nd.sum(Z * nd.expand_dims(R/Zs, 1), axis=2)
 
-    # numpy functions that are not yet translated to mxnet:
-    # ####################################################
-    #
-    # def _flat_lrp(self,R):
-    #     '''
-    #     distribute relevance for each output evenly to the output neurons' receptive fields.
-    #     note that for fully connected layers, this results in a uniform lower layer relevance map.
-    #     '''
-    #     Z = np.ones_like(self.W[na,:,:])
-    #     Zs = Z.sum(axis=1)[:,na,:]
-    #     return ((Z / Zs) * R[:,na,:]).sum(axis=2)
-    #
-    # def _ww_lrp(self,R):
-    #     '''
-    #     LRR according to Eq(12) in https://arxiv.org/pdf/1512.02479v1.pdf
-    #     '''
-    #     Z = self.W[na,:,:]**2
-    #     Zs = Z.sum(axis=1)[:,na,:]
-    #     return ((Z / Zs) * R[:,na,:]).sum(axis=2)
-    #
-    # def _epsilon_lrp(self,R,epsilon):
-    #     '''
-    #     LRP according to Eq(58) in DOI: 10.1371/journal.pone.0130140
-    #     '''
-    #     Z = self.W[na,:,:]*self.X[:,:,na] # localized preactivations
-    #     Zs = Z.sum(axis=1)[:,na,:] +self.B[na,na,:] # preactivations
-    #
-    #     # add slack to denominator. we require sign(0) = 1. since np.sign(0) = 0 would defeat the purpose of the numeric stabilizer we do not use it.
-    #     Zs += epsilon * ((Zs >= 0)*2-1)
-    #     return ((Z / Zs) * R[:,na,:]).sum(axis=2)
-    #
-    #
-    # def _alphabeta_lrp(self,R,alpha):
-    #     '''
-    #     LRP according to Eq(60) in DOI: 10.1371/journal.pone.0130140
-    #     '''
-    #     beta = 1 - alpha
-    #     Z = self.W[na,:,:]*self.X[:,:,na] # localized preactivations
-    #
-    #     if not alpha == 0:
-    #         Zp = Z * (Z > 0)
-    #         Zsp = Zp.sum(axis=1)[:,na,:] + (self.B * (self.B > 0))[na,na,:]
-    #         Ralpha = alpha * ((Zp / Zsp) * R[:,na,:]).sum(axis=2)
-    #     else:
-    #         Ralpha = 0
-    #
-    #     if not beta == 0:
-    #         Zn = Z * (Z < 0)
-    #         Zsn = Zn.sum(axis=1)[:,na,:] + (self.B * (self.B < 0))[na,na,:]
-    #         Rbeta = beta * ((Zn / Zsn) * R[:,na,:]).sum(axis=2)
-    #     else:
-    #         Rbeta = 0
-    #
-    #     return Ralpha + Rbeta
+
+
+    def _flat_lrp(self,R):
+        '''
+        distribute relevance for each output evenly to the output neurons' receptive fields.
+        note that for fully connected layers, this results in a uniform lower layer relevance map.
+        '''
+        Z = nd.ones(nd.expand_axis(self.W, 0).shape)
+        Zs = nd.expand_axis(nd.sum(Z, axis=1), axis=1)
+        return nd.sum(((Z / Zs) * nd.expand_axis(R, axis=1)), axis=2)
+
+
+    def _ww_lrp(self,R):
+        '''
+        LRR according to Eq(12) in https://arxiv.org/pdf/1512.02479v1.pdf
+        '''
+        Z = nd.expand_dims(self.W**2, axis=0)
+        Zs = nd.expand_axis(nd.sum(Z, axis=1), axis=1)
+        return nd.sum(((Z / Zs) * nd.expand_axis(R, axis=1)), axis=2)
+
+
+    def _epsilon_lrp_slow(self,R,epsilon):
+        '''
+        LRP according to Eq(58) in DOI: 10.1371/journal.pone.0130140
+        This function shows all necessary operations to perform LRP in one place and is therefore not optimized
+        '''
+
+        Z = nd.expand_dims(self.W, axis=0) * nd.expand_dims(self.X, axis=2) #localized preactivations
+        Zs = nd.expand_dims(nd.sum(Z, axis=1), axis=1) + nd.expand_dims(nd.expand_dims(self.B, axis=0), axis=0) # preactivations
+
+        # add slack to denominator. we require sign(0) = 1. since np.sign(0) = 0 would defeat the purpose of the numeric stabilizer we do not use it.
+        Zs += epsilon * ((Zs >= 0)*2-1)
+        return ((Z / Zs) * R[:,na,:]).sum(axis=2)
+
+
+    def _epsilon_lrp(self,R,epsilon):
+        '''
+        LRP according to Eq(58) in DOI: 10.1371/journal.pone.0130140
+        '''
+
+        Zs = self.Y + epsilon * ((self.Y >= 0)*2-1)#prepare stabilized denominator
+
+        # Has the forward pass been computed lrp-aware?
+        # This exchanges time spent in the forward pass for lower LRP time
+        # and is useful, if e.g. several parameter settings for LRP need to be evaluated
+        # for the same input data.
+        if self.lrp_aware:
+            return nd.sum(self.Z * nd.expand_dims(R/Zs, 1), axis=2)
+        else:
+            Z = nd.expand_dims(self.W, axis=0) * nd.expand_dims(self.X, axis=2) #localized preactivations
+            return nd.sum(Z * nd.expand_dims(R/Zs, 1), axis=2)
+
+
+    def _alphabeta_lrp_slow(self,R,alpha):
+        '''
+        LRP according to Eq(60) in DOI: 10.1371/journal.pone.0130140
+        This function shows all necessary operations to perform LRP in one place and is therefore not optimized
+        '''
+        beta = 1 - alpha
+        Z = nd.expand_dims(self.W, axis=0) * nd.expand_dims(self.X, axis=2) #localized preactivations
+
+        if not alpha == 0:
+            Zp = Z * (Z > 0)
+            Zsp = nd.expand_dims(nd.sum(Zp, axis=1), axis=1) + nd.expand_dims(nd.expand_dims(self.B * (self.B > 0), axis=0), axis=0)
+            Ralpha = alpha * nd.sum(((Zp / Zsp) * nd.expand_dims(R, axis=1)), axis=2)
+        else:
+            Ralpha = 0
+
+        if not beta == 0:
+            Zn = Z * (Z < 0)
+            Zsn = nd.expand_dims(nd.sum(Zn, axis=1), axis=1) + nd.expand_dims(nd.expand_dimx(self.B * (self.B < 0), axis=0), axis=0)
+            Rbeta = beta * nd.sum(((Zn / Zsn) * nd.expand_dims(R, axis=1)), axis=2)
+        else:
+            Rbeta = 0
+
+        return Ralpha + Rbeta
+
+
+    def _alphabeta_lrp(self,R,alpha):
+        '''
+        LRP according to Eq(60) in DOI: 10.1371/journal.pone.0130140
+        '''
+        beta = 1 - alpha
+        if self.lrp_aware:
+            Z = self.Z
+        else:
+            Z = nd.expand_dims(self.W, axis=0) * nd.expand_dims(self.X, axis=2) #localized preactivations
+
+
+        #index mask of positive forward predictions
+        Zplus = Z > 0
+        if alpha * beta != 0: #the general case: both parameters are not 0
+            Zp = Z * Zplus
+            Zsp = nd.sum(Zp, axis=1) + nd.expand_dims(self.B * (self.B > 0), axis=0) + 1e-16
+
+            Zn = Z - Zp
+            Zsn = self.Y - Zsp - 1e-16
+
+            return alpha * nd.sum(Zp * nd.expand_dims(R/Zsp, axis=1), axis=2) + beta * nd.sum(Zn * nd.expand_dims(R/Zsn, axis=1), axis=2)
+
+        elif alpha: #only alpha is not 0 -> alpha = 1, beta = 0
+            Zp = Z * Zplus
+            Zsp = nd.sum(Zp, axis=1) + nd.expand_dims(self.B * (self.B > 0), axis=0) + 1e-16
+            return nd.sum(Zp * nd.expand_dims(R/Zsp, axis=1), axis=2)
+
+        elif beta: # only beta is not 0 -> alpha = 0, beta = 1
+            Zn = Z <= 0 # ist this correct? Z * np.invert(Zplus)
+            Zsn = nd.sum(Zn, axis=1) + nd.expand_dims(self.B * (self.B < 0), axis=0) - 1e-16
+            return nd.sum(Zn * nd.expand_dims(R/Zsn, axis=1), axis=2)
+
+        else:
+            raise Exception('This case should never occur: alpha={}, beta={}.'.format(alpha, beta))
