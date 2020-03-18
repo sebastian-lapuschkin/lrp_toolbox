b5c1b989113b8d60a30825eace3816f7b89a5164
max <maximilian.kohlbrenner@gmx.de>
2020-03-18 15:13:12 +0100

    Remove unused demo files for singularity

--- singularity/lrp_demo.sh
+++
@@ -1,4 +0,0 @@
-# singularity call to calculate heatmaps according to the config stored in the
-# lrp_demo folder
-
-singularity run -B lrp_demo:/opt/lrp_toolbox/caffe-master-lrp/demonstrator/lrp_demo_files/ caffe-lrp-cpu-u16.04.sif -c ./lrp_demo_files/config.txt -f ./lrp_demo_files/testfilelist.txt -p ./lrp_demo_files
